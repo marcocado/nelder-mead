dccfbd417de87ae7fcaa1b5f2670748c1cf8eec6
Marco <109109273+marcocado@users.noreply.github.com>
2023-02-14 21:33:35 +0100

    Update animation.py

--- animation.py
+++ animation.py
@@ -6,10 +6,12 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 if __name__ == "__main__":
+    # read data with pandas and convert it to a numpy array
     df = pd.read_csv(r'/home/marco/Projekte/NelderMead/downhill_simplex.txt', sep=", ", engine='python')
     data = df.to_numpy()
     length = data.shape[0]
 
+    # create a scatter plot for the himmelblau function to visualize the downhill simplex process
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(9, 9), dpi=160)
     ax.view_init(elev=60, azim=45, roll=0)
     x = np.arange(-6, 0, 0.05)
@@ -19,6 +21,9 @@ if __name__ == "__main__":
     surf = ax.scatter(x, y, f, c=f, alpha=0.14)
 
     def animate(i):
+    '''
+    Animation function
+    '''
         x = data[i, [0, 3, 6]]
         y = data[i, [1, 4, 7]]
         f = np.power((np.power(x, 2) + y - 11), 2) + np.power((x + np.power(y, 2) - 7), 2)
