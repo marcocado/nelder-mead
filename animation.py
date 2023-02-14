aff05d295f03b76b0612c0faf243cd68986e06f1
Marco <109109273+marcocado@users.noreply.github.com>
2023-02-14 19:40:56 +0100

    Add files via upload

---
+++ animation.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+if __name__ == "__main__":
+    df = pd.read_csv(r'/home/marco/Projekte/NelderMead/downhill_simplex.txt', sep=", ", engine='python')
+    data = df.to_numpy()
+    length = data.shape[0]
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(9, 9), dpi=160)
+    ax.view_init(elev=60, azim=45, roll=0)
+    x = np.arange(-6, 0, 0.05)
+    y = np.arange(-6, 0, 0.05)
+    x, y = np.meshgrid(x, y)
+    f =  np.power((np.power(x, 2) + y - 11), 2) + np.power((x + np.power(y, 2) - 7), 2)
+    surf = ax.scatter(x, y, f, c=f, alpha=0.14)
+
+    def animate(i):
+        x = data[i, [0, 3, 6]]
+        y = data[i, [1, 4, 7]]
+        f = np.power((np.power(x, 2) + y - 11), 2) + np.power((x + np.power(y, 2) - 7), 2)
+        verts = [list(zip(x,y,f))]
+        ax.add_collection3d(Poly3DCollection(verts, facecolors='grey', edgecolor="black", alpha=0.4))
+
+    anim = animation.FuncAnimation(fig, animate, frames=length)
+    writergif = animation.PillowWriter(fps=0.6) 
+    anim.save("downhill_simplex.gif", writer=writergif)
