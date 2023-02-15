28d956a9932a3e20a0fab5f3cbc9262313cbbd71
Marco <109109273+marcocado@users.noreply.github.com>
2023-02-15 15:57:04 +0100

    Update animation.py
    Changed the name of the numpy array to a more accurate one

--- animation.py
+++ animation.py
@@ -8,7 +8,7 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 if __name__ == "__main__":
     # read data with pandas and convert it to a numpy array
     df = pd.read_csv(r'/home/marco/Projekte/NelderMead/downhill_simplex.txt', sep=", ", engine='python')
-    data = df.to_numpy()
+    coordinate_array = df.to_numpy()
     length = data.shape[0]
 
     # create a scatter plot for the himmelblau function to visualize the downhill simplex process
@@ -24,8 +24,8 @@ if __name__ == "__main__":
     '''
     Animation function
     '''
-        x = data[i, [0, 3, 6]]
-        y = data[i, [1, 4, 7]]
+        x = coordinate_array[i, [0, 3, 6]]
+        y = coordinate_array[i, [1, 4, 7]]
         f = np.power((np.power(x, 2) + y - 11), 2) + np.power((x + np.power(y, 2) - 7), 2)
         verts = [list(zip(x,y,f))]
         ax.add_collection3d(Poly3DCollection(verts, facecolors='grey', edgecolor="black", alpha=0.4))
